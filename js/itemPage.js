e449a0282c421e867f50ee62bd5f5ec56bdb7fcf
Jakaran <80308625+jaskaranproductions@users.noreply.github.com>
2021-11-09 17:05:16 +0530

    item page

---
+++ js/itemPage.js
@@ -0,0 +1,15 @@
+var data = [
+  {
+    img: "https://cdn.shopify.com/s/files/1/1743/7443/products/1_19ae3dcc-01c1-4482-9630-8a72e1f538a7_270x270.jpg?v=1621436676",
+    off: "10",
+    brand: "Wet N Wild",
+    name: "Wet N Wild Megalast Liquid Catsuit Matte Lipstick",
+    rating: "4",
+    review: "80",
+    cutPrice: "599",
+    price: 539,
+    disc: "Wet N Wild's lipstick formula contains all the same things as the other guys: rich color, vitamins A & E, aloe vera, and really feels like putting silk on your lips. If you're already thinking That is a crazy bargain, we've now added macadamia nut oil that contains antioxidants to hydrate your lips for a smooth, supple pout. Are we just crazy? Have we gone too far? Or do you deserve it? We definitely think so! One try, and we guarantee you will declare this your best lipstick brand of life.",
+  },
+];
+var container = document.querySelector(".product-display");
+
